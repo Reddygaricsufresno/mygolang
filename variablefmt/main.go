1b53d75173aa4dee717e0c2a0acd08788ffdf40f
Reddygaricsufresno <bharghava_reddy_reddygari@mail.fresnostate.edu>
2016-02-01 02:27:00 -0800

    first commit

---
+++ variablefmt/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	integerType := 10
+	booleanType := true
+	stringType := "hello world"
+	floatType := 10.001
+	fmt.Printf("%T\n", integerType)
+	fmt.Printf("%T\n", booleanType)
+	fmt.Printf("%T\n", stringType)
+	fmt.Printf("%T\n", floatType)
+
+}
