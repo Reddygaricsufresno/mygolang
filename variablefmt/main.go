7721bf1024e1cab342a104258e5221f1d8b15cab
Reddygaricsufresno <bharghava_reddy_reddygari@mail.fresnostate.edu>
2016-02-01 02:37:11 -0800

    first commit

--- variablefmt/main.go
+++ variablefmt/main.go
@@ -6,7 +6,7 @@ func main() {
 	integerType := 10
 	booleanType := false
 	stringType := "hello world"
-	floatType := 10.001
+	floatType := 10.00001
 	fmt.Printf("%T\n", integerType)
 	fmt.Printf("%T\n", booleanType)
 	fmt.Printf("%T\n", stringType)
